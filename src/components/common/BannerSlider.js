ab7dd51c967b10d94f5201f2b26de65687cb44d9
Tuvshinjargal <tuvshuutuvshinjargal21@gmail.com>
2026-03-08 22:37:20 +0800

    fix header title clamp

--- src/components/common/BannerSlider.js
+++ src/components/common/BannerSlider.js
@@ -257,7 +257,7 @@ export default function BannerSlider({
                     }}
                   >
                     <h3
-                      className="text-2xl font-bold mb-3 mr-8 line-clamp-3"
+                      className="text-2xl font-bold mb-3 mr-8"
                       style={{
                         writingMode: "vertical-rl",
                       }}
@@ -265,7 +265,7 @@ export default function BannerSlider({
                       {image.caption.title}
                     </h3>
                     <p
-                      className="text-gray-200 mr-8 line-clamp-3"
+                      className="text-gray-200 mr-8"
                       style={{
                         writingMode: "vertical-rl",
                       }}
